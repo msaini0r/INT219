e41f563ebaf61b06c24e581936db22e3e0ca5f8c
msaini0r <mohit.12012540@lpu.in>
2022-09-14 00:03:14 +0530

    events

---
+++ lecture-9 [ Event-Listener ]/colorevent.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = "salmon";
+
+  // document.body.style.color = 'white';
+});
